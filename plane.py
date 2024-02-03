98143fcf448817a5d32f0086f8b32c3299ae9e43
Šimon Bernard <113086082+SimonBer7@users.noreply.github.com>
2024-02-03 20:34:18 +0100

    Delete plane.py

--- plane.py
+++
@@ -1,8 +0,0 @@
-
-class Plane:
-    def __init__(self, name, type, capacity, range):
-        self.name = name
-        self.type = type
-        self.capacity = capacity
-        self.range = range
-
